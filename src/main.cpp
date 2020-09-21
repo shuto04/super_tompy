4ef509ed20457cd093cb19e47bdc6bba357e4146
Naoki Numaguchi <naoki.numaguchi@groove-x.com>
2020-09-21 21:20:29 +0900

    add reset command

--- src/main.cpp
+++ src/main.cpp
@@ -8,32 +8,36 @@
 #define PATTERN_LEN 16
 
 typedef enum {
-    PauseWithBase = 0,
-    PauseWithTarget,
+    Pause = 0,
     Play,
     NumState
 } State;
-State state = PauseWithBase;
+State state = Pause;
 
 uint8_t beat0[SERVO_NUM][BEAT_LEN] = {{1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0},
                                       {1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0},
                                       {0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0}};
-uint8_t beat1[SERVO_NUM][BEAT_LEN] = {{1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0},
-                                      {0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0}};
-uint8_t patterns[PATTERN_LEN] = {0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1};
+uint8_t beat1[SERVO_NUM][BEAT_LEN] = {{1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0},
+                                      {0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0},
+                                      {0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0}};
+// uint8_t patterns[PATTERN_LEN] = {0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1};
+// uint8_t patterns[PATTERN_LEN] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+uint8_t patterns[PATTERN_LEN] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
 
 int beatIndex = 0;
 int patternIndex = 0;
 
-// int beatInterval = 125; // 120 bpm -> 2 beat / 1sec -> 500msec / 4つ打ち (500 / 4) = 125
-int beatInterval = 176; // 85 bpm
-// int beatInterval = 250; // 60 bpm
-// int beatInterval = 500; // 30 bpm
+// int beatInterval = (60. / 120.) / 4 * 1000; // 120 bpm -> 2 beat / 1sec -> 500msec / 4つ打ち (500 / 4) = 125
+// int beatInterval = (60. / 85.) / 4 * 1000;  // 85 bpm 
+int beatInterval = (60. / 81.4) / 4 * 1000; // 81.4 bpm 
+// int beatInterval = (60. / 60.) / 4 * 1000;  // 60 bpm
+// int beatInterval = (60. / 30.) / 4 * 1000;  // 30 bpm
 
 unsigned long lastUpdate;
 unsigned long printLastUpdate;
+bool lastWasReset = false;
 
-RhythmServo servos[SERVO_NUM] = {RhythmServo(1, beatInterval, 95, Minus), 
+RhythmServo servos[SERVO_NUM] = {RhythmServo(1, beatInterval, 90, Minus), 
                                  RhythmServo(3, beatInterval, 95, Plus),
                                  RhythmServo(5, beatInterval, 90, Plus)};
 
@@ -76,11 +80,13 @@ void servo_update()
 void setup() {
     M5.begin(true, false, true);
     M5.Lcd.println("Super tompy");
-    M5.Lcd.println("Start by Left button");
+    M5.Lcd.println("BtnA: Start/Pause");
+    M5.Lcd.println("BtnB: Pause and set base/target position");
+    M5.Lcd.println("BtnC: Pause and reset");
 
     Wire.begin(21, 22, 100000);
 
-    servo_reset();
+    servo_set();
     delay(1000);
 }
 
@@ -98,11 +104,9 @@ void display()
                 servos[i].BasePos(), servos[i].TargetAng(), servos[i].RotateDirection());
         }
         if (state == Play)
-            M5.Lcd.println("Play           ");
-        else if(state == PauseWithBase)
-            M5.Lcd.println("PauseWithBase  ");
-        else if(state == PauseWithTarget)
-            M5.Lcd.println("PauseWithTarget");        
+            M5.Lcd.println("Play ");
+        else if(state == Pause)
+            M5.Lcd.println("Pause");        
         M5.Lcd.printf("patternIndex: %3d\n", patternIndex);
         M5.Lcd.printf("beatIndex: %3d\n", beatIndex);
 
@@ -117,6 +121,22 @@ void input()
     {
         state = State((state + 1) % NumState);
     }
+    if(M5.BtnB.wasPressed())
+    {
+        state = Pause;
+        if(lastWasReset)
+            servo_set();
+        else
+            servo_reset();
+        lastWasReset = !lastWasReset;
+    }
+    if(M5.BtnC.wasPressed())
+    {
+        state = Pause;
+        servo_reset();
+        beatIndex = 0;
+        patternIndex = 0;
+    }
 }
 
 void update()
@@ -124,12 +144,6 @@ void update()
     if (state == Play)
     {
         servo_update();
-    }else if(state == PauseWithBase)
-    {
-        servo_reset();
-    }else if(state == PauseWithTarget)
-    {
-        servo_set();
     }
 }
 
